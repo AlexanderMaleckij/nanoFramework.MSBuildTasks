ab3ac113cea5edbe278794a30957855eb369b719
Alexander Maleckij <52193641+AlexanderMaleckij@users.noreply.github.com>
2023-06-17 14:37:41 +0400

    Add project files.

---
+++ src/nanoFramework.MSBuildTasks/Models/EmbedResourcesTaskItem.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+using Microsoft.Build.Framework;
+
+namespace nanoFramework.MSBuildTasks.Models
+{
+    internal sealed class EmbedResourcesTaskItem
+    {
+        private const string REGEX_FILTER_METADATA_NAME = "RegexFilter";
+        private const string MetadataRequiredTemplate = "\"{0}\" metadata is required.";
+        private const string DirectoryDoesNotExistTemplate = "Directory \"{0}\" does not exist.";
+
+        public string AbsoluteFolderPath { get; }
+
+        public Regex RegexFilter { get; }
+
+        public EmbedResourcesTaskItem(string projectDirectory, ITaskItem taskItem)
+        {
+            if (taskItem is null)
+            {
+                throw new ArgumentNullException(nameof(taskItem));
+            }
+
+            var folderPath = taskItem.ItemSpec;
+            var pattern = taskItem.GetMetadata(REGEX_FILTER_METADATA_NAME);
+
+            if (string.IsNullOrWhiteSpace(pattern))
+            {
+                throw new ArgumentException(
+                    string.Format(MetadataRequiredTemplate, REGEX_FILTER_METADATA_NAME));
+            }
+
+            var absoluteFolderPath = GetAbsolutePath(folderPath, projectDirectory);
+
+            if (!Directory.Exists(absoluteFolderPath))
+            {
+                throw new ArgumentException(
+                    string.Format(DirectoryDoesNotExistTemplate, folderPath));
+            }
+
+            AbsoluteFolderPath = absoluteFolderPath;
+            RegexFilter = new Regex(pattern);
+        }
+
+        static string GetAbsolutePath(string path, string basePath)
+        {
+            if (Path.IsPathRooted(path))
+            {
+                return path;
+            }
+            else
+            {
+                return Path.GetFullPath(Path.Combine(basePath, path));
+            }
+        }
+    }
+}
---
+++ src/nanoFramework.MSBuildTasks/Tasks/GenerateResx.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Resources;
+
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+using nanoFramework.MSBuildTasks.Models;
+
+namespace nanoFramework.MSBuildTasks.Tasks
+{
+    public sealed class GenerateResx : Task
+    {
+        [Required]
+        public string ProjectDirectory { get; set; }
+
+        [Required]
+        public string ResxFileName { get; set; }
+
+        [Required]
+        public bool ResxUseForwardSlashes { get; set; }
+
+        [Required]
+        public ITaskItem[] Sources { get; set; }
+
+        public override bool Execute()
+        {
+            EmbedResourcesTaskItem[] sources = null;
+
+            try
+            {
+                sources = Sources.Select(x => new EmbedResourcesTaskItem(ProjectDirectory, x)).ToArray();
+            }
+            catch (Exception e)
+            {
+                Log.LogErrorFromException(e);
+
+                return false;
+            }
+
+            var resourceWriter = new ResXResourceWriter(ResxFileName);
+
+            foreach (var source in sources)
+            {
+                var fullFilesPathsToInclude = Directory.GetFiles(source.AbsoluteFolderPath, "*", SearchOption.AllDirectories)
+                    .Where(path => source.RegexFilter.IsMatch(path))
+                    .ToArray();
+
+                foreach (var fullFilePathToInclude in fullFilesPathsToInclude)
+                {
+                    var relativeFilePath = fullFilePathToInclude.Substring(source.AbsoluteFolderPath.Length + 1);
+
+                    var resourceName = ResxUseForwardSlashes
+                        ? relativeFilePath.Replace('\\', '/')
+                        : relativeFilePath;
+
+                    var fileRef = new ResXFileRef(fullFilePathToInclude, typeof(byte[]).AssemblyQualifiedName);
+                    var dataNode = new ResXDataNode(resourceName, fileRef);
+
+                    resourceWriter.AddResource(dataNode);
+                }
+            }
+
+            resourceWriter.Generate();
+            resourceWriter.Close();
+
+            return true;
+        }
+    }
+}
